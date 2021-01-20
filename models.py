88f45cb44c2363c14b3d37185945d9dd9959f610
cadilow <cadilow@mail.ru>
2021-01-20 19:33:35 +0300

    v1

---
+++ models.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    mail = db.Column(db.String, CheckConstraint("email LIKE '%@%'"), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    orders = db.relationship('Order', back_populates='users')
+
+
+dishes_orders_association = db.Table(
+    'dishes_orders',
+    db.Column('dish_id', db.Integer, db.ForeignKey('dishes.id')),
+    db.Column('order_id', db.Integer, db.ForeignKey('orders.id'))
+)
+
+
+class Dish(db.Model):
+    __tablename__ = 'dishes'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String, nullable=False)
+    category = db.relationship('Category', back_populates='dishes')
+    orders = db.relationship('Order', secondary=dishes_orders_association, back_populates='dishes')
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    meals = db.relationship('Dish', back_populates='categories')
+    dish_id = db.Column(db.Integer, db.ForeignKey('dishes.id'), nullable=False)
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    total = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, CheckConstraint("email LIKE '%@%'"), nullable=False)
+    tel = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    basket = db.Column(db.String, nullable=False)
+    owner = db.relationship('User', back_populates='orders')
+    dishes = db.relationship('Dish', secondary=dishes_orders_association, back_populates='orders')
+
